ed302efafa013332c3fdcbae846bdc4493c8b8db
diego_goncalves <ddiegofg@gmail.com>
2014-05-22 22:21:35 -0300

    buildpath fixed

--- ASKIT/src/com/example/askit/MainActivity.java
+++ ASKIT/src/com/example/askit/MainActivity.java
@@ -10,6 +10,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import com.facebook.*;
+import com.facebook.model.*;
 
 public class MainActivity extends ActionBarActivity {
 
